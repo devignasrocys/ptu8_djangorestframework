2c5da5e40c9ec165404506d3369f27b0a2a64a4d
devignasrocys <devignasrocys@gmail.com>
2023-03-06 11:49:29 +0200

    another urls update

--- ptu8_restframework/restframeworkapp/urls.py
+++ ptu8_restframework/restframeworkapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
      path('', views.BandsList.as_view()),
      path('albums/', views.AlbumsList.as_view()),
      path('songs/', views.SongsList.as_view()),
-     path('albums_reviews/', views.AlbumReviewList.as_view()),
-     path('albums_reviews_comments/', views.AlbumReviewCommentList.as_view()),
-     path('albums_reviews_likes/', views.AlbumReviewLikeList.as_view()),
+     path('albums/reviews/', views.AlbumReviewList.as_view()),
+     path('albums/reviews/comments/', views.AlbumReviewCommentList.as_view()),
+     path('albums/reviews/likes/', views.AlbumReviewLikeList.as_view()),
 ]
